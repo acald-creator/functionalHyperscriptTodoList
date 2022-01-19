a03ba2f7f3657876e9faa987049bdf60928064fb
Antonette Caldwell <pullmana8@gmail.com>
2022-01-19 15:21:23 -0600

    Removed 'hot' reload, webpack dev does it for you automatically

--- webpack.dev.js
+++ webpack.dev.js
@@ -10,7 +10,6 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   devtool: 'inline-source-map',
   devServer: {
-    hot: true, // Tell the dev-server we're using HMR
     static: {
       directory: path.resolve(__dirname, 'dist'),
       publicPath: '/'
